f81d689cdb47cae198db4624f3eee60dc62afeb7
PCWitcher <pcwitcher.ie@gmail.com>
2019-11-11 09:41:14 +0000

    fix API

--- src/components/views/Trip/Trip.js
+++ src/components/views/Trip/Trip.js
@@ -15,7 +15,7 @@ import styles from './Trip.scss';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import OrderForm from '../../features/OrderForm/OrderFormContainer';
 
-const Trip = ({ error, name, image, cost, days, description, country, intro }) => {
+const Trip = ({ error, name, image, cost, days, description, country, intro, id }) => {
   if (error) return <NotFound />;
   else return (
     <Section>
@@ -45,7 +45,7 @@ const Trip = ({ error, name, image, cost, days, description, country, intro }) =
           <Col xs={12}>
             <PageTitle text='Trip details' />
             {HTMLParser(description)}
-            <OrderForm tripCost={cost} />
+            <OrderForm tripCost={cost} tripName={name} tripId={id} countryCode={country.alpha3Code} />
           </Col>
         </Row>
       </Grid>
